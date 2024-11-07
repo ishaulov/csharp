e8b48e409c8a9bb4ade529f32ea2d499e8894773
Ilya Shaulov <ilya.shaulov@yandex.ru>
2024-11-07 21:42:25 +0500

    change Program.cs

--- myfirstapp/Program.cs
+++ myfirstapp/Program.cs
@@ -4,25 +4,7 @@
     {
         static void Main(string[] args)
         {
-
             Console.WriteLine("Hello, C#!");
-
-            var sum1 = StaticSum(6, 7);
-            Console.WriteLine("sum1 = " + sum1);
-
         }
-
-        public static int StaticSum(int a, int b)
-        {
-
-            var sum = a + b;
-
-            return sum;
-        }
-    }
-
-    public class MyFirstClass
-    {
-        public static void OopsMethod(){}
     }
 }
