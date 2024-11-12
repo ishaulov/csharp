14c6fefc3a2d311401766eb0b1d7a2d2005302b7
Ilya Shaulov <shaulov@macroscop.com>
2024-11-12 18:39:32 +0500

    оптимизация замеров (stopwatch)

--- myfirstapp/Program.cs
+++ myfirstapp/Program.cs
@@ -8,7 +8,7 @@ namespace myfirstapp
 		static void Main(string[] args)
 		{
 			listExample();
-			
+
 			arrayList();
 
 			LinkedList();
@@ -18,9 +18,8 @@ namespace myfirstapp
 		{
 			var rand = new Random();
 			var list = new List<int>();
-			
-			var stopwatch = new Stopwatch();
-			stopwatch.Start();
+
+			var stopwatch = Stopwatch.StartNew();
 
 			for (int i = 0; i < 1000000; i++)
 			{
@@ -29,31 +28,31 @@ namespace myfirstapp
 
 			stopwatch.Stop();
 			Console.WriteLine($"Time for generate list of elements: {stopwatch.ElapsedMilliseconds} ms");
-			
-			var stopwatch1 = new Stopwatch();
-			stopwatch1.Start();
-			Console.WriteLine(list[496753]);
-			stopwatch1.Stop();
-			Console.WriteLine($"Time for found element 496753: {stopwatch1.ElapsedMilliseconds} ms");
 
-			var stopwatch2 = new Stopwatch();
-			stopwatch2.Start();
+			stopwatch.Reset();
+			stopwatch.Start();
+			Console.WriteLine(list[496753]);
+			stopwatch.Stop();
+			Console.WriteLine($"Time for found element 496753: {stopwatch.ElapsedMilliseconds} ms");
+
+			stopwatch.Reset();
+			stopwatch.Start();
 			foreach (int value in list)
 			{
 				if (value % 777 == 0)
 					Console.WriteLine($"Value is: {value}");
 			}
-			stopwatch2.Stop();
-			Console.WriteLine($"Time for /777: {stopwatch2.ElapsedMilliseconds} ms");
+
+			stopwatch.Stop();
+			Console.WriteLine($"Time for /777: {stopwatch.ElapsedMilliseconds} ms");
 		}
 
 		public static void arrayList()
 		{
 			var rand = new Random();
 			var arrList = new ArrayList();
-			
-			var stopwatch = new Stopwatch();
-			stopwatch.Start();
+
+			var stopwatch = Stopwatch.StartNew();
 
 			for (int i = 0; i < 1000000; i++)
 			{
@@ -62,31 +61,31 @@ namespace myfirstapp
 
 			stopwatch.Stop();
 			Console.WriteLine($"Time for generate arraylist of elements: {stopwatch.ElapsedMilliseconds} ms");
-			
-			var stopwatch1 = new Stopwatch();
-			stopwatch1.Start();
-			Console.WriteLine(arrList[496753]);
-			stopwatch1.Stop();
-			Console.WriteLine($"Time for found element 496753 of arraylist: {stopwatch1.ElapsedMilliseconds} ms");
 
-			var stopwatch2 = new Stopwatch();
-			stopwatch2.Start();
+			stopwatch.Reset();
+			stopwatch.Start();
+			Console.WriteLine(arrList[496753]);
+			stopwatch.Stop();
+			Console.WriteLine($"Time for found element 496753 of arraylist: {stopwatch.ElapsedMilliseconds} ms");
+
+			stopwatch.Reset();
+			stopwatch.Start();
 			foreach (int value in arrList)
 			{
 				if (value % 777 == 0)
 					Console.WriteLine($"Value is: {value}");
 			}
-			stopwatch2.Stop();
-			Console.WriteLine($"Time for /777 of arraylist: {stopwatch2.ElapsedMilliseconds} ms");		
+
+			stopwatch.Stop();
+			Console.WriteLine($"Time for /777 of arraylist: {stopwatch.ElapsedMilliseconds} ms");
 		}
-		
+
 		public static void LinkedList()
 		{
 			var rand = new Random();
 			var linkedList = new LinkedList<int>();
-			
-			var stopwatch = new Stopwatch();
-			stopwatch.Start();
+
+			var stopwatch = Stopwatch.StartNew();
 
 			for (int i = 0; i < 1000000; i++)
 			{
@@ -95,22 +94,24 @@ namespace myfirstapp
 
 			stopwatch.Stop();
 			Console.WriteLine($"Time for generate linkedList of elements: {stopwatch.ElapsedMilliseconds} ms");
-			
-			var stopwatch1 = new Stopwatch();
-			stopwatch1.Start();
-			Console.WriteLine(linkedList.ElementAt(496753));
-			stopwatch1.Stop();
-			Console.WriteLine($"Time for found element 496753 of linkedList: {stopwatch1.ElapsedMilliseconds} ms");
 
-			var stopwatch2 = new Stopwatch();
-			stopwatch2.Start();
+			stopwatch.Reset();
+			stopwatch.Start();
+			Console.WriteLine(linkedList.ElementAt(496753));
+			stopwatch.Stop();
+			Console.WriteLine($"Time for found element 496753 of linkedList: {stopwatch.ElapsedMilliseconds} ms");
+
+			stopwatch.Reset();
+			stopwatch.Start();
+
 			foreach (int value in linkedList)
 			{
 				if (value % 777 == 0)
 					Console.WriteLine($"Value is: {value}");
 			}
-			stopwatch2.Stop();
-			Console.WriteLine($"Time for /777 of arraylist: {stopwatch2.ElapsedMilliseconds} ms");		
+
+			stopwatch.Stop();
+			Console.WriteLine($"Time for /777 of arraylist: {stopwatch.ElapsedMilliseconds} ms");
 		}
 	}
 }
