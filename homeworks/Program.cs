8aaac88b6a1f3b29b377aea49eb9ed58568f19bb
Ilya Shaulov <shaulov@macroscop.com>
2024-11-19 18:43:41 +0500

    вроде файнайл

--- homeworks/Program.cs
+++ homeworks/Program.cs
@@ -6,7 +6,8 @@
 		public static void Main(string[] args)
 		{
 			var arguments = new Quadratic();
-			arguments.SetArguments(args);
+			arguments.SetArguments();
+
 
 
 			// d = Discriminant(a, b, c);
@@ -21,56 +22,6 @@
 			// }
 		}
 
-		public static int Discriminant(int a, int b, int c)
-		{
-			int d;
-
-			try
-			{
-				d = checked(b * b - 4 * a * c);
-			}
-			catch (Exception ex)
-			{
-				Console.WriteLine("Exception: \n" + ex);
-				return 0;
-			}
-
-			return d;
-		}
-
-		public static List<double> XFinder(int a, int b, int d)
-		{
-			double x1 = 0;
-			double x2 = 0;
-			List<double> result = new List<double>();
-			try
-			{
-				if (d > 0)
-				{
-					x1 = (-b + Math.Sqrt(d)) / 2 * a;
-					x2 = (-b - Math.Sqrt(d)) / 2 * a;
-					result.Add(x1);
-					result.Add(x2);
-				}
-				else if (d == 0)
-				{
-					x1 = (-b + Math.Sqrt(d)) / 2 * a;
-					result.Add(x1);
-				}
-				else
-				{
-					Console.BackgroundColor = ConsoleColor.Yellow;
-					Console.ForegroundColor = ConsoleColor.Black;
-					throw new Exclude("Вещественных значений не найдено");
-				}
-			}
-			catch (Exception e)
-			{
-				Console.WriteLine("Bright lies \n" + e);
-				throw;
-			}
-
-			return result;
-		}
+		
 	}
 }
