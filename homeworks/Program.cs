c72fe74a7079dbccb867269c2484f4f0d0348c6d
Ilya Shaulov <shaulov@macroscop.com>
2024-11-22 20:25:06 +0500

    changes!

--- homeworks/Program.cs
+++ homeworks/Program.cs
@@ -5,7 +5,28 @@
 	{
 		public static void Main(string[] args)
 		{
+			List<string?> inputValues = new List<string?>();
+			while (true)
+			{
+				string input = Console.ReadLine();
+				if (input != "")
+				{
+					inputValues.Add(input);
+				}
+				else
+				{
+					Console.WriteLine("Result:");
+					break;
+				}
+			}
+
+			Stack stack = new Stack(inputValues);
+			// stack.Add("ll");
+			// stack.Add("xx");
+			// stack.Pop();
 			
+			Console.WriteLine($"Count of stack elements: " + stack.Size);
+			Console.WriteLine($"Last element of stack: " + stack.Top);
 		}
 	}
 }
