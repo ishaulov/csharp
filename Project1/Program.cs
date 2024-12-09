181905ec7f9e052896d85783d36d41d7870023a8
Ilya Shaulov <shaulov@macroscop.com>
2024-12-09 19:06:55 +0500

    add Program.cs

---
+++ Project1/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Project1;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		// Console.WriteLine("Hello, World!");
+
+		var Venus = new
+		{
+			Name = "Venus",
+			orderNumberFromTheSun = 2,
+			equatorLength = 38025
+		};
+
+		var Earth = new
+		{
+			Name = "Earth",
+			orderNumberFromTheSun = 3,
+			equatorLength = 40075,
+			previousPlanet = Venus
+		};
+
+		var Mars = new
+		{
+			Name = "Mars",
+			orderNumberFromTheSun = 4,
+			equatorLength = 21344,
+			previousPlanet = Earth
+		};
+
+		Console.WriteLine(Venus);
+		Console.WriteLine(Earth);
+	}
+}
