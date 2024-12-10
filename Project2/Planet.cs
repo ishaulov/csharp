b733b5f6ed8e733895a7507016b19a36fee471e8
Ilya Shaulov <shaulov@macroscop.com>
2024-12-10 19:57:48 +0500

    add Project2

---
+++ Project2/Planet.cs
@@ -0,0 +1,53 @@
+namespace Project2;
+
+public class Planet
+{
+	private string _name = string.Empty;
+	private int _orderNumberFromTheSun = 0;
+	private int _equatorLength = 0;
+
+	public string Name
+	{
+		get
+		{
+			return _name;
+		}
+		set
+		{
+			_name = value;
+		}
+	}
+
+	public int OrderNumberFromTheSun
+	{
+		get
+		{
+			return _orderNumberFromTheSun;
+		}
+		set
+		{
+			_orderNumberFromTheSun = value;
+		}
+	}
+
+	public int EquatorLength
+	{
+		get
+		{
+			return _equatorLength;
+		}
+
+		set
+		{
+			_equatorLength = value;
+		}
+	}
+
+	public Planet(string name, int orderNumberFromTheSun, int equatorLength)
+	{
+		Name = name;
+		OrderNumberFromTheSun = orderNumberFromTheSun;
+		EquatorLength = equatorLength;
+		
+	}
+}
