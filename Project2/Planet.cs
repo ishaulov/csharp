623cb9a7cb34b7789b41e11db6c980f35f638560
tiunov <qwestern13@yandex.ru>
2024-12-12 17:35:35 +0500

    Previous planet... mrakobesie & jazz

--- Project2/Planet.cs
+++ Project2/Planet.cs
@@ -2,49 +2,51 @@ namespace Project2;
 
 public class Planet
 {
-	private string _name = string.Empty;
-	private int _orderNumberFromTheSun = 0;
-	private int _equatorLength = 0;
-	private string _previousPlanet = "null";
-	private string _previousPlanetCor = "null";
-	
-	
+    private string _name = string.Empty;
+    private int _orderNumberFromTheSun = 0;
+    private int _equatorLength = 0;
+    private string _previousPlanet = string.Empty;
 
-	public string Name
-	{
-		get { return _name; }
-		set { _name = value; }
-	}
-
-	public int OrderNumberFromTheSun
-	{
-		get { return _orderNumberFromTheSun; }
-		set { _orderNumberFromTheSun = value; }
-	}
-
-	public int EquatorLength
-	{
-		get { return _equatorLength; }
-
-		set { _equatorLength = value; }
-	}
-
-	public string PreviousPlanet
-	{
-		get { return _previousPlanet; }
-		set
-		{
-			_previousPlanetCor = value;
-			
-		}
-	}
+    private List<string> _planetsList = new List<string> { "Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"};
 
 
-	public Planet(string name, int orderNumberFromTheSun, int equatorLength)
-	{
-		Name = name;
-		OrderNumberFromTheSun = orderNumberFromTheSun;
-		EquatorLength = equatorLength;
-		PreviousPlanet = name;
-	}
+public string Name
+    {
+        get { return _name; }
+        set { _name = value; }
+    }
+
+    public int OrderNumberFromTheSun
+    {
+        get { return _orderNumberFromTheSun; }
+        set { _orderNumberFromTheSun = value; }
+    }
+
+    public int EquatorLength
+    {
+        get { return _equatorLength; }
+
+        set { _equatorLength = value; }
+    }
+
+    public string PreviousPlanet
+    {
+        get { return _previousPlanet; }
+        set { _previousPlanet = value; }
+    }
+
+    public Planet(string name, int orderNumberFromTheSun, int equatorLength)
+    {
+        Name = name;
+        OrderNumberFromTheSun = orderNumberFromTheSun;
+        EquatorLength = equatorLength;
+        if (orderNumberFromTheSun == 1)
+        {
+            PreviousPlanet = "SUN!!!";
+        }
+        else
+        {
+            PreviousPlanet = _planetsList[orderNumberFromTheSun - 2];
+        }
+    }
 }
--- Project2/Program.cs
+++ Project2/Program.cs
@@ -12,8 +12,12 @@ class Program
 
 		Planet venus = new Planet("Venus", 2, 36000);
 		Planet earth = new Planet("Earth", 3, 42000);
+		Planet mercury = new Planet("Mercury", 1, 42000);
+		Planet neptune = new Planet("Neptune", 8, 42000);
 		
 		Console.WriteLine(venus.PreviousPlanet);
 		Console.WriteLine(earth.PreviousPlanet);
+		Console.WriteLine(mercury.PreviousPlanet);
+		Console.WriteLine(neptune.PreviousPlanet);
 	}
 }
