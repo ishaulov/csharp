d0c6e9bbb241e7c004a3b68ca4d9e18ec9709573
Ilya Shaulov <shaulov@macroscop.com>
2024-12-12 11:18:39 +0500

    add csproj

---
+++ Project2/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Project2;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		/*CatalogOfPlanets catalog = new CatalogOfPlanets();
+		Console.WriteLine(catalog.GetPlanet("Venus"));
+		Console.WriteLine(catalog.GetPlanet("Limonia"));
+		catalog.GetPlanet("Venus");
+		catalog.GetPlanet("Venus");*/
+
+		Planet venus = new Planet("Venus", 2, 36000);
+		Planet earth = new Planet("Earth", 3, 42000);
+		
+		Console.WriteLine(venus.PreviousPlanet);
+		Console.WriteLine(earth.PreviousPlanet);
+	}
+}
---
+++ Project3/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Project3;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		CatalogOfPlanets catalog = new CatalogOfPlanets();
+		Console.WriteLine(catalog.GetPlanet("Venus"));
+		Console.WriteLine(catalog.GetPlanet("Limonia"));
+		catalog.GetPlanet("Venus");
+		catalog.GetPlanet("Venus");
+
+	}
+}
